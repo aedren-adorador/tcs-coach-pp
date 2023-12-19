71ea9c11008b461e2ba062e95e2e4fecfa42c9c4
200082 <aedren.adorador@obf.ateneo.edu>
2023-12-19 11:46:19 +0800

    additional auth changes

--- src/auth/auth-subcomponents/create-account-auth/RegisterEmail.js
+++ src/auth/auth-subcomponents/create-account-auth/RegisterEmail.js
@@ -1,6 +1,7 @@
 import React from "react";
 import '../../auth-styles/auth.styles.css';
-import { Input, Button } from "@chakra-ui/react";
+import { Input, Button,  FormControl, FormLabel } from "@chakra-ui/react";
+import { Formik, Form} from "formik";
 import { ArrowBackIcon } from '@chakra-ui/icons'
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
@@ -11,14 +12,43 @@ function RegisterEmail() {
     useEffect(() => {
         setRevealed(true);
     }, []);
-    
+
+    const handleSubmit = (e) => {
+        console.log('HELLO! NICE')
+    }
+
     return(
         <>
         <div className={`auth-big-container page-reveal ${revealed ? 'reveal' : ''}`}>
             <div id="auth-register-email-container">
-                <Input variant='filled' placeholder='Enter email' mb='2' size='lg' style={{ height: '70px'}}/>
-                <Button float='right' colorScheme='green' size='lg'>Verify Email</Button>
-                <Link to='/'><Button float='left' colorScheme='blackAlpha' size='sm' variant='ghost'><ArrowBackIcon/>&nbsp;Go Back</Button></Link>
+                <Formik
+                initialValues={{email: ''}}
+                onSubmit={handleSubmit}
+                >
+                {(formikProps) => (
+                <Form>   
+                    <FormControl>
+                        <FormLabel>Email Address</FormLabel>
+                            <Input
+                            variant='filled'
+                            placeholder='Enter email'
+                            mb='2'
+                            size='lg'
+                            style={{ height: '70px'}}
+                            />
+                            <Button
+                            float='right'
+                            colorScheme='green'
+                            size='lg'
+                            type='submit'
+                            >Verify Email</Button>
+                            <Link to='/'>
+                                <Button float='left' colorScheme='blackAlpha' size='sm' variant='ghost'><ArrowBackIcon/>&nbsp;Go Back</Button>
+                            </Link>
+                    </FormControl>
+                </Form> 
+                )}
+                </Formik>
             </div>
         </div>
         </>
