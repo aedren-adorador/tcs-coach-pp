d30caf8abe0276faf3ba7a40c2ac33143f23f82a
200082 <aedren.adorador@obf.ateneo.edu>
2023-12-24 12:32:34 +0800

    added navbutton icons and functionalities

--- src/nav/Nav.js
+++ src/nav/Nav.js
@@ -1,7 +1,25 @@
-import { Box, Button, Flex, Text} from "@chakra-ui/react";
-import React from "react";
+import { Avatar, Box, Button, Flex, Text} from "@chakra-ui/react";
+import React, { useEffect, useState } from "react";
+import tcsLogo from '../tcs-logo.jpeg'
+import { AppstoreAddOutlined, AppstoreFilled, AppstoreOutlined, BulbFilled, BulbOutlined, EditOutlined, FormOutlined, LogoutOutlined, MessageFilled, MessageOutlined, ProfileFilled, ProfileOutlined, QuestionCircleFilled, QuestionCircleOutlined, QuestionOutlined } from "@ant-design/icons";
 
 function Nav({logOut}) {
+    const navButtons = ['My Applications', 'TCS Job Openings', 'Frequently Asked Qs', 'Learn About TCS', 'Contact Us']
+    const navButtonSVGs = [
+        <ProfileOutlined />, <AppstoreOutlined />, <QuestionCircleOutlined />, <BulbOutlined />, <MessageOutlined />
+    ]
+    const navClickedButtonSVGs = [
+        <ProfileFilled />,<AppstoreFilled />, <QuestionCircleFilled />, <BulbFilled />, <MessageFilled />
+    ]
+    const [clickedNavButton, setClickedNavButton] = useState('')
+    
+    const handleNavButtonClick = (index) => {
+        setClickedNavButton(navButtons[index])
+    }
+
+    useEffect(()=> {
+    },[clickedNavButton])
+    
     return(
         <>
         <Box
@@ -13,38 +31,40 @@ function Nav({logOut}) {
         width='250px'
         height='100%'
         >
+            {navButtons.map((button, index) => {
+                if (button !== clickedNavButton) {
+                    return<Button
+                    justifyContent='flex-start'
+                    fontWeight='500'
+                    variant='ghost'
+                    colorScheme="black"
+                    width='240px'
+                    mb='1'
+                    key={index}
+                    onClick={() => handleNavButtonClick(index)}
+                    >
+                       { navButtonSVGs[index]}&nbsp;&nbsp;{button}
+                    </Button>
+                } else {
+                    return <Button
+                    color='teal'
+                    fontWeight='600'
+                    width='240px'
+                    mb='1'
+                    key={index}
+                    onClick={() => handleNavButtonClick(index)}
+                    >{ navClickedButtonSVGs[index]}&nbsp;{button}
+                    </Button>
+                }
+            })}
             <Button
-            variant='outline'
-            colorScheme="blue"
-            width='240px'
-            mb='1'
-            >My Applications
-            </Button>
-
-            <Button
-            variant='outline'
-            colorScheme="blue"
-            width='240px'
-            mb='1'
-            >
-            TCS Job Openings
-            </Button>
-           
-           <Button
-            variant='outline'
-            colorScheme="blue"
-            width='240px'
-            mb='1'
-            >
-            Frequently Asked Qs
-            </Button>
-            <Button
+            justifyContent='flex-start'
             variant='ghost'
             colorScheme="red"
             width='240px'
             onClick={logOut}
             >
-            Logout
+            {<LogoutOutlined />}&nbsp;&nbsp;Logout
             </Button>
         </Box>
         </>
